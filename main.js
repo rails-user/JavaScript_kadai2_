8eed25b026ab823e3c95953e566dba86b58f3f86
なおゆき <nao@USERnoMacBook-puro.local>
2021-04-05 17:29:40 +0900

    再レビューの指摘取り込み版

--- main.js
+++ main.js
@@ -1,29 +1,31 @@
-function buttonClick(){
-	
-	const FizzNum = document.getElementById('FizzNum');	
-	const BuzzNum = document.getElementById('BuzzNum');	
+document.getElementById('execBtn').onclick = function(){
+
+	const fizzNum = document.getElementById('fizzNum');	
+	const buzzNum = document.getElementById('buzzNum');	
 	const pattern = new RegExp(/^[1-9]\d?$/);
+	const parseFizz = parseInt(fizzNum.value);
+	const parseBuzz = parseInt(buzzNum.value);
 	
 	items.innerHTML = '';
 	
-	for(let i=1; i<100; i++) {
+	for (let i = 1; i < 100; i++) {
 	
 		const item = document.createElement('li');
 	
 		//FizzNumとBuzzNumに入力された値が半角整数値かチェック
-		if (pattern.test(FizzNum.value) && pattern.test(BuzzNum.value)) {
-			if (i%FizzNum.value == 0 && i%BuzzNum.value == 0) {
-				item.textContent = 'FizzBuzz' + i;
+		if (pattern.test(fizzNum.value) && pattern.test(buzzNum.value)) {
+			if (i%parseFizz === 0 && i%parseBuzz === 0) {
+				item.textContent = `FizzBuzz${i}`;
 				items.appendChild(item);
-			} else if (i%FizzNum.value == 0) {
-				item.textContent = 'Fizz' + i;
+			} else if (i%parseFizz === 0) {
+				item.textContent = `Fizz${i}`;
 				items.appendChild(item);
-			} else if (i%BuzzNum.value == 0) {
-				item.textContent = 'Buzz' + i;
+			} else if (i%parseBuzz === 0) {
+				item.textContent = `Buzz${i}`;
 				items.appendChild(item);
 			}
 		} else {
-			item.textContent = '整数値を入力してください'
+			item.textContent = `整数値を入力してください`;
 			items.appendChild(item);	
 			break;
 		}
